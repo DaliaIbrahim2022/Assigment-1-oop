729549ad31e73c419f2c2d6fa7b199ca350740ad
Nervana-Milad <101606226+Nervana-Milad@users.noreply.github.com>
2022-10-25 18:24:39 +0200

    modify problem one

--- main.cpp
+++ main.cpp
@@ -1,99 +1,47 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define besmellah ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#include<iostream>
+#include<string>
+#include<sstream>
 using namespace std;
-template <class T>
-class Darray
-{
-private:
-    T* ptr;
-    int length;
-    int  n;
-public:
-    Darray()
+int main() {
+    string s, s2;
+    cout << "Enter a sentence: " << endl;
+    getline(cin, s);
+    stringstream ss(s);
+    while (ss >> s2)
     {
-        ptr = new T[n];
-        cout<<"enter size of Array : ";
-        cin>>n;
-        length = n;
-        for(int i=0;i<n;i++)
+        for (int i = 0; i < s2.size(); i++)
         {
-            ptr[i]=0;
-        }
-    }
-    Darray(int n)
-    {
-        ptr = new T[n];
-        length=n;
-        for(int i=0;i<n;i++)
-        {
-            ptr[i]=0;
-        }
+            if (s2[i] == 'h' && s2[i + 1] == 'i' && s2[i + 2] == 'm')
+            {
+                if (!isalpha(s2[i + 3]))
+                {
+                    s2 = s2.substr(0, 3) + " or her" + s2.substr(i + 3, s2.size());
 
+                }
+            }
+        }
+        for (int i = 0; i < s2.size(); i++)
+        {
+            if (s2[i] == 'H' && s2[i + 1] == 'e')
+            {
+                if (!isalpha(s2[i + 2]))
+                {
+                    s2 = s2.substr(0, 2) + " or she" + s2.substr(i + 2, s2.size());
 
-    }
-    void getarray()
-    {
-        cout<<"enter the array`s value : ";
-        for(int i=0;i<length;i++)
+                }
+            }
+        }
+        for (int i = 0; i < s2.size(); i++)
         {
-            cin>>ptr[i];
-        }
-    }
-    Darray(const Darray& anotherarray){
-        delete []ptr;
-        length=anotherarray.length;
-        ptr = new T[length];
-        for (int i = 0; i < length; i++) {
-            ptr[i] = anotherarray.ptr[i];
-        }
-    }//copy constructor
-    T& operator[](int index)
-    {
-        if (index >= length) {
-            cout<<" index is out of boundary "<<endl;
-        }
-        return ptr[index];
-    }
-    T& operator =(const Darray& anotherarray)
-    {
-        length = anotherarray.length;
-        delete []ptr;
-        ptr = new T[length];
-        for (int i = 0; i < length; i++)
-        {
-            ptr[i] = anotherarray.ptr[i];
-        }
-    }
-    int size()
-    {
-        return length;
-    }
-    void print()
-    {
-        cout<<"values is : "<<endl;
-        for(int i=0;i<length;i++)
-        {
-            cout<<ptr[i]<<" ";
-        }
-    }
-    ~Darray();
-};
-//_________________________
-template<class T>
-Darray<T>::~Darray() {
-    delete[] ptr;
-}
-int main()
-{
-    Darray<char> a;
-    a.getarray();
-    Darray <char> b(a);
-    //b.getarray();
-    // b[1]='g';
-    //b=a;
-    cout<<"size of array is :" <<b.size()<<endl;
-    cout<<"the value of index is : "<<b[1]<<endl;
-    b.print();
+            if (s2[i] == 'h' && s2[i + 1] == 'e')
+            {
+                if (!isalpha(s2[i + 2]))
+                {
+                    s2 = s2.substr(0, 2) + " or she" + s2.substr(i + 2, s2.size());
+                }
+            }
 
+        }
+        cout << s2 << " ";
+    }
 }
